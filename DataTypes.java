87cce556b5a90f86a47c44163709afa7150f6d62
risa-o <risa.orsinger@gmail.com>
2019-09-08 10:41:21 -0500

    Completed HackerRank Day 1:Datatypes

--- DataTypes.java
+++ DataTypes.java
@@ -4,27 +4,30 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class DataTypes {
-	
+public class Solution {
+    
     public static void main(String[] args) {
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
-		
+        
         Scanner scan = new Scanner(System.in);
 
         /* Declare second integer, double, and String variables. */
-
         /* Read and save an integer, double, and String to your variables.*/
         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        int num = scan.nextInt();
+        double num2 = scan.nextDouble();
+        scan.nextLine();
+        String words = scan.nextLine();
 
         /* Print the sum of both integer variables on a new line. */
-
+        System.out.println( i + num);
         /* Print the sum of the double variables on a new line. */
-		
+        System.out.println ( d + num2);
         /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
-
+            the 's' variable above should be printed first. */
+        System.out.println (s + words);
         scan.close();
     }
 }
